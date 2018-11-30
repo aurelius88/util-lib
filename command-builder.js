7af732c7149915998c7beb6fbdc505871e316658
Aurelius <aurelius.dev@gmail.com>
2018-11-30 18:28:20 +0100

    Initial commit
    + chat-helper for print messages in chat (and in console)
    + file-helper for reading/writing json files and more
    + hook-manager for managing several hooks in groups
    + command-builder - work in progress
    + update manifest

---
+++ command-builder.js
@@ -0,0 +1,28 @@
+
+
+class CommandBuilder{
+
+    constructor() {
+        this.commands = {};
+
+    }
+
+    _getSubCommand( subCommand ) {
+        let parts = subCommand ? subCommand.split(".") : [];
+        let result = this.commands;
+        for(let part of parts) {
+            if(part) result = result[part];
+        }
+        return result;
+    }
+
+    addCommand( commandName, commandCallback, subCommand="" ) {
+        this._getSubCommand(subCommand)[commandName] = commandCallback;
+        this._creatHelp(commandName, subCommand);
+    }
+
+    _createHelp( commandName, subCommand="" ) {
+        subCommand = subCommand ? "help."+subCommand : "help";
+        let helpCmd = this._getSubCommand(subCommand)[commandName];
+    }
+}
