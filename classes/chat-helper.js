b3db7400b2c1f28ef9950be19b2ae69adf0a7811
Aurelius <aurelius.dev@gmail.com>
2018-12-12 16:10:59 +0100

    Add colorByValue function and make methods static again
    Due to changes for "NextGen Proxy" and it is more flexible to have 
    "classes" (although there are no real classes in JS) rather than objects 
    in the UtilLib object, some methods become static again. For 
    compatibility reasons, the non-static methods remain and just delegates 
    to the static methods.

--- classes/chat-helper.js
+++ classes/chat-helper.js
@@ -1,3 +1,59 @@
+const binarySearch = require( "binary-search" );
+
+function _colorByValueMap( value, valueColorMap ) {
+    let colorSteps = Array.from( valueColorMap.keys() );
+    colorSteps.sort( _compareNumbers );
+    let max = colorSteps[colorSteps.length - 1];
+    let min = colorSteps[0];
+    let currentStep = value > max ? max : value < min ? min : value;
+    if ( currentStep === min || currentStep === max ) {
+        return valueColorMap.get( currentStep );
+    } else {
+        let local = _localMinMax( colorSteps, currentStep, _compareNumbers );
+        let minColor = valueColorMap.get( local.min );
+        let maxColor = valueColorMap.get( local.max );
+        let relVal = 1 - ( local.max - currentStep ) / ( local.max - local.min );
+        let resColor = [];
+        for ( let i = 0; i < minColor.length; i++ ) {
+            resColor[i] = ( maxColor[i] - minColor[i]) * relVal - minColor[i];
+        }
+        return resColor;
+    }
+}
+
+function _localMinMax( array, value, comparator ) {
+    let localMinIndex = binarySearch( array, value, comparator );
+    if ( localMinIndex < 0 ) localMinIndex = ~localMinIndex;
+    let localMin = array[localMinIndex];
+    let localMax = array[localMinIndex < array.length - 1 ? localMinIndex + 1 : localMinIndex];
+    return { min: localMin, max: localMax };
+}
+
+function _colorByValueObject( value, valueColorMap ) {
+    let colorSteps = Object.keys( valueColorMap );
+    colorSteps.sort( _compareNumbers );
+    let max = colorSteps[colorSteps.length - 1];
+    let min = colorSteps[0];
+    let currentStep = value > max ? max : value < min ? min : value;
+    if ( currentStep === min || currentStep === max ) {
+        return valueColorMap[currentStep];
+    } else {
+        let local = _localMinMax( colorSteps, currentStep, _compareNumbers );
+        let minColor = valueColorMap[local.min];
+        let maxColor = valueColorMap[local.max];
+        let relVal = 1 - ( local.max - currentStep ) / ( local.max - local.min );
+        let resColor = [];
+        for ( let i = 0; i < minColor.length; i++ ) {
+            resColor[i] = ( maxColor[i] - minColor[i]) * relVal - minColor[i];
+        }
+        return resColor;
+    }
+}
+
+function _compareNumbers( a, b ) {
+    return a - b;
+}
+
 class ChatHelper {
     constructor( mod ) {
         this.COLOR_ENABLE = "#56B4E9";
@@ -6,6 +62,18 @@ class ChatHelper {
         this.COLOR_VALUE = "#09d1d1";
         this.COLOR_HIGHLIGHT = "#81ee7b";
         this.mod = mod;
+    }
+
+    static colorByValue( value, valueColorMap ) {
+        let type = valueColorMap.toString();
+        if ( !valueColorMap || !["object", "Map"].includes( type ) )
+            throw new Error( "Cannot generate a color without color map (Map or object). [value -> color]" );
+        if ( type === "object" ) {
+            if ( Object( valueColorMap ).keys().length ) return _colorByValueObject( value, valueColorMap );
+        } else {
+            if ( valueColorMap.size ) return _colorByValueMap( value, valueColorMap );
+        }
+        throw new Error( "There should be at least 1 value to color mapping." );
     }
 
     /**
@@ -27,8 +95,13 @@ class ChatHelper {
      * @static
      * @memberOf OutputHelper
      */
-    cleanString( dirtyString ) {
+    static cleanString( dirtyString ) {
         return dirtyString.replace( /<[^>]*>/g, "" );
+    }
+
+    // delegates to ChatHelper.cleanString
+    cleanString( dirtyString ) {
+        return ChatHelper.cleanString(dirtyString);
     }
 
     /**
@@ -38,7 +111,7 @@ class ChatHelper {
      * @static
      * @memberOf OutputHelper
      */
-    msToUTCTimeString( timeInMs ) {
+    static msToUTCTimeString( timeInMs ) {
         let secs = Math.floor( timeInMs / 1000.0 ),
             mins = Math.floor( secs / 60.0 ),
             h = Math.floor( mins / 60.0 ),
@@ -50,6 +123,11 @@ class ChatHelper {
         return `${h}:${m}:${s}`;
     }
 
+    // delegates to ChatHelper.msToUTCTimeString
+    msToUTCTimeString( timeInMs ) {
+        return ChatHelper.msToUTCTimeString( timeInMs );
+    }
+
     /**
      * Adds a zero to numbers smaller than 10.
      * @param {[type]} num The number to be formatted.
@@ -58,11 +136,16 @@ class ChatHelper {
      * @static
      * @memberOf OutputHelper
      */
-    addPrefixZero( num ) {
+    static addPrefixZero( num ) {
         if ( num < 10 ) {
             num = "0" + num;
         }
         return num;
+    }
+
+    // delegates to ChatHelper.addPrefixZero
+    addPrefixZero( num ) {
+        return ChatHelper.addPrefixZero( num );
     }
 }
 
