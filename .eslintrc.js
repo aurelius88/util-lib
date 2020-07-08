ccd7eb450fbd98f4cee9341b0fedfeb0a6fb1f9b
Aurelius <aurelius.dev@gmail.com>
2020-07-08 12:26:38 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
     env: {
-        es6: true,
+        es2020: true,
         node: true
     },
     extends: "eslint:recommended",
     parserOptions: {
-        ecmaVersion: 2017,
+        ecmaVersion: 2020,
         sourceType: "script",
         ecmaFeatures: {
             impliedStrict: true
