d3e345ee407db2214ff0b1a222624d267653fb92
Aurelius <aurelius.dev@gmail.com>
2022-08-12 04:08:39 +0200

    Add contract data

---
+++ data-src/contract.ts
@@ -0,0 +1,12 @@
+export enum Types {
+    parcel_post = 8,
+    vendor_gold = 9,
+    teleport = 15,
+    vendor_triumph_or_angler_token = 20,
+    storage = 26,
+    vendor_reputation_credits = 49,
+    vendor_bellacarium = 50,
+    vendor_ace_medals = 80,
+    vendor_guild = 83,
+    fish_delivery = 93,
+}
---
+++ data/contract.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Types = void 0;
+var Types;
+(function (Types) {
+    Types[Types["parcel_post"] = 8] = "parcel_post";
+    Types[Types["vendor_gold"] = 9] = "vendor_gold";
+    Types[Types["teleport"] = 15] = "teleport";
+    Types[Types["vendor_triumph_or_angler_token"] = 20] = "vendor_triumph_or_angler_token";
+    Types[Types["storage"] = 26] = "storage";
+    Types[Types["vendor_reputation_credits"] = 49] = "vendor_reputation_credits";
+    Types[Types["vendor_bellacarium"] = 50] = "vendor_bellacarium";
+    Types[Types["vendor_ace_medals"] = 80] = "vendor_ace_medals";
+    Types[Types["vendor_guild"] = 83] = "vendor_guild";
+    Types[Types["fish_delivery"] = 93] = "fish_delivery";
+})(Types = exports.Types || (exports.Types = {}));
